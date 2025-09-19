d8b5661c659ed18b8e5a399efdbeaac6b068b94a
Nguyen Hoang Dat <datnhce180797@fpt.edu.vn>
2025-09-19 17:41:47 +0700

    first commit - set up project

---
+++ database/script.sql
@@ -0,0 +1,228 @@
+create table authors
+(
+    id     bigint auto_increment
+        primary key,
+    name   varchar(100) not null,
+    bio    text         null,
+    active tinyint      not null
+);
+
+create table promotions
+(
+    id            bigint auto_increment
+        primary key,
+    content       text        not null,
+    type          varchar(50) not null,
+    promotion_day date        not null
+);
+
+create table publishers
+(
+    id      bigint auto_increment
+        primary key,
+    name    varchar(100) not null,
+    address varchar(255) null,
+    active  tinyint      not null
+);
+
+create table series
+(
+    id          bigint auto_increment
+        primary key,
+    name        varchar(200)                        not null,
+    description text                                null,
+    author      varchar(100)                        null,
+    created_at  timestamp default CURRENT_TIMESTAMP null,
+    updated_at  timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
+);
+
+create table sup_catergories
+(
+    id     int          not null
+        primary key,
+    name   varchar(100) null,
+    active tinyint(1)   not null
+);
+
+create table sub_categories
+(
+    id          bigint auto_increment
+        primary key,
+    sup_cat_id  int          null,
+    name        varchar(100) not null,
+    description text         null,
+    active      tinyint(1)   not null,
+    constraint sub_categories_sup_catergories_id_fk
+        foreign key (sup_cat_id) references sup_catergories (id)
+);
+
+create table books
+(
+    id             bigint auto_increment
+        primary key,
+    category_id    bigint        null,
+    title          varchar(200)  not null,
+    description    text          null,
+    price          double        not null,
+    stock_quantity int default 0 null,
+    published_date date          null,
+    active         tinyint       null,
+    author_id      bigint        null,
+    publisher_id   bigint        null,
+    constraint books_author_fk
+        foreign key (author_id) references authors (id),
+    constraint books_ibfk_1
+        foreign key (category_id) references sub_categories (id)
+            on delete set null,
+    constraint books_publisher_fk
+        foreign key (publisher_id) references publishers (id)
+);
+
+create index category_id
+    on books (category_id);
+
+create table series_books
+(
+    series_id bigint not null,
+    book_id   bigint not null,
+    primary key (series_id, book_id),
+    constraint series_books_ibfk_1
+        foreign key (series_id) references series (id),
+    constraint series_books_ibfk_2
+        foreign key (book_id) references books (id)
+);
+
+create table users
+(
+    id        varchar(36) charset utf8mb3 not null
+        primary key,
+    username  varchar(50)                 not null,
+    email     varchar(100)                not null,
+    password  varchar(255)                not null,
+    full_name varchar(100)                null,
+    phone     varchar(20)                 null,
+    address   varchar(255)                null,
+    active    tinyint(1)                  not null comment 'Active flag for the user (soft delete when set tinyint(0))',
+    constraint email
+        unique (email),
+    constraint username
+        unique (username)
+);
+
+create table cart_items
+(
+    id         bigint auto_increment
+        primary key,
+    user_id    varchar(36) charset utf8mb3         not null,
+    book_id    bigint                              null,
+    quantity   int                                 not null,
+    created_at timestamp default CURRENT_TIMESTAMP null,
+    deleted_at timestamp                           null,
+    constraint cart_items_ibfk_2
+        foreign key (book_id) references books (id),
+    constraint cart_items_ibfk_3
+        foreign key (user_id) references users (id)
+);
+
+create index book_id
+    on cart_items (book_id);
+
+create table orders
+(
+    id           bigint auto_increment
+        primary key,
+    user_id      varchar(36) charset utf8mb3                                                null,
+    status       enum ('PENDING', 'PAID', 'SHIPPED', 'CANCELLED') default 'PENDING'         null,
+    total_amount double                                                                     not null,
+    address      varchar(50) charset utf8mb3                                                null,
+    created_at   timestamp                                        default CURRENT_TIMESTAMP null,
+    deleted_at   timestamp                                                                  null,
+    constraint orders_ibfk_1
+        foreign key (user_id) references users (id)
+);
+
+create table bills
+(
+    id           bigint auto_increment
+        primary key,
+    order_id     bigint                              null,
+    bill_number  varchar(50)                         not null,
+    total_amount decimal(12, 2)                      not null,
+    tax          double    default 0                 null,
+    created_at   timestamp default CURRENT_TIMESTAMP null,
+    deleted_at   timestamp                           null,
+    constraint bill_number
+        unique (bill_number),
+    constraint order_id
+        unique (order_id),
+    constraint bills_ibfk_1
+        foreign key (order_id) references orders (id)
+);
+
+create table order_items
+(
+    id         bigint auto_increment
+        primary key,
+    order_id   bigint                              null,
+    book_id    bigint                              null,
+    quantity   int                                 not null,
+    price      double                              null,
+    created_at timestamp default CURRENT_TIMESTAMP null,
+    deleted_at timestamp                           null,
+    constraint order_items_ibfk_1
+        foreign key (order_id) references orders (id),
+    constraint order_items_ibfk_2
+        foreign key (book_id) references books (id)
+);
+
+create index book_id
+    on order_items (book_id);
+
+create index order_id
+    on order_items (order_id);
+
+create table payments
+(
+    id         bigint auto_increment
+        primary key,
+    order_id   bigint                                                          null,
+    method     enum ('COD', 'MOMO', 'VNPAY', 'BANK_TRANSFER')                  null,
+    status     enum ('PENDING', 'SUCCESS', 'FAILED') default 'PENDING'         null,
+    paid_at    timestamp                                                       null,
+    created_at timestamp                             default CURRENT_TIMESTAMP null,
+    deleted_at timestamp                                                       null,
+    constraint order_id
+        unique (order_id),
+    constraint payments_ibfk_1
+        foreign key (order_id) references orders (id)
+);
+
+create table reviews
+(
+    id         bigint auto_increment
+        primary key,
+    user_id    varchar(36) charset utf8mb3         null,
+    book_id    bigint                              null,
+    rating     int                                 null,
+    comment    text                                null,
+    created_at timestamp default CURRENT_TIMESTAMP null,
+    constraint reviews_ibfk_1
+        foreign key (user_id) references users (id),
+    constraint reviews_ibfk_2
+        foreign key (book_id) references books (id),
+    check (`rating` between 1 and 5)
+);
+
+create index book_id
+    on reviews (book_id);
+
+create table user_roles
+(
+    user_id varchar(36) charset utf8mb3 not null,
+    role    varchar(10)                 not null,
+    primary key (user_id, role),
+    constraint user_roles_ibfk_1
+        foreign key (user_id) references users (id)
+);
+
+
