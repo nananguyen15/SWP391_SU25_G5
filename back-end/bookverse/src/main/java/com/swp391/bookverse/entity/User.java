cd9638f6c453e4d3270d594860791dd9e1ae872d
nhant93 <nhant9324w@gmail.com>
2025-10-14 08:33:13 +0700

    Update chuc nang

--- back-end/bookverse/src/main/java/com/swp391/bookverse/entity/User.java
+++ back-end/bookverse/src/main/java/com/swp391/bookverse/entity/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -35,7 +35,7 @@ public class User {
     boolean active;
 
     @ElementCollection
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
     Set<String> roles;
 }
